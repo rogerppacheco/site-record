8784f5ad5a30a03399b7fb9dc8f2baf4b676fa87
rogerppacheco <roggerio@gmail.com>
2026-01-07 01:27:21 -0300

    Permite acesso público temporário para endpoint bônus M-10 (AllowAny)

--- gestao_equipes/settings.py
+++ gestao_equipes/settings.py
@@ -172,7 +172,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     # ✅ PAGINAÇÃO PARA PERFORMANCE
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
